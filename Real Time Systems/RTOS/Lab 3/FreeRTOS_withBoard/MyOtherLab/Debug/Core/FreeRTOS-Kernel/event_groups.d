cbc4770651b337e3bda2a95d652e76c180a747b7
Shup04 <Bradleyschmidt04@gmail.com>
2025-01-30 20:39:47 -0800

    Organized more

---
+++ Real Time Systems/RTOS/Lab 3/FreeRTOS_withBoard/MyOtherLab/Debug/Core/FreeRTOS-Kernel/event_groups.d
@@ -0,0 +1,28 @@
+Core/FreeRTOS-Kernel/event_groups.o: \
+ ../Core/FreeRTOS-Kernel/event_groups.c \
+ C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/FreeRTOS.h \
+ C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/FreeRTOSConfig.h \
+ C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/projdefs.h \
+ C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/portable.h \
+ C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/deprecated_definitions.h \
+ C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/mpu_wrappers.h \
+ C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/task.h \
+ C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/list.h \
+ C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/timers.h \
+ C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/task.h \
+ C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/event_groups.h \
+ C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/timers.h
+C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/FreeRTOS.h:
+C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/FreeRTOSConfig.h:
+C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/projdefs.h:
+C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/portable.h:
+C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/deprecated_definitions.h:
+C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/mpu_wrappers.h:
+C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/task.h:
+C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/list.h:
+C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/timers.h:
+C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/task.h:
+C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/event_groups.h:
+C:/Users/darkt/OneDrive\ -\ Thompson\ Rivers\ University/Documents/New\ 3020\ Material/Labs/MyOtherLab/Core/FreeRTOS-Kernel/include/timers.h:
